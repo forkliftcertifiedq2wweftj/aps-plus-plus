07e73b018532f314071428541c7f6c998b25d282
forkliftcertifiedq2wweftj <joaomiguel28032015@gmail.com>
2024-08-25 22:49:37 -0300

    Update config.js

--- server/config.js
+++ server/config.js
@@ -181,7 +181,7 @@ module.exports = {
     // The possible nest food types that can spawn.
     FOOD_TYPES_NEST: [
         [1, [
-            [32, 'pentagon'], [ 8, 'betaPentagon'], [ 2, 'alphaPentagon'], [ 1.5, 'shape8']
+            [32, 'pentagon'], [ 8, 'betaPentagon'], [ 2, 'alphaPentagon']
         ]]
     ],
 
