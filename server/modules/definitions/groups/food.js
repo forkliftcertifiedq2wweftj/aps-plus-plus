568f42aeeb196433ed65edc6e9323387fb6f1eca
forkliftcertifiedq2wweftj <joaomiguel28032015@gmail.com>
2024-09-06 00:01:58 -0300

    Update food.js

--- server/modules/definitions/groups/food.js
+++ server/modules/definitions/groups/food.js
@@ -433,7 +433,7 @@ Class.s7 = {
     VALUE: 26260,
     SHAPE: 7,
     SIZE: 70,
-    COLOR: #c7c461,
+    COLOR: 'seven',
     BODY: {
         DAMAGE: 1.5 * basePolygonDamage,
         DENSITY: 8,
